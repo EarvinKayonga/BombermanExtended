f1c42e53094780c6349084052d7fa1d181ffc27c
EarvinKayonga <demoneartin@gmail.com>
2017-05-28 12:13:10 +0200

    gameloop beside windowloop

--- client/client.c
+++ client/client.c
@@ -1,6 +1,7 @@
 #include 	    "client.h"
 
-int             game_loop(int);
+int             window_loop(int);
+int             game_loop(SDL_Surface*, int, int, client_t, fd_set, struct timeval);
 
 int             main(int argc, char **args)
 {
@@ -10,148 +11,160 @@ int             main(int argc, char **args)
     configuration = from_arguments(argc, args);
     socket = create_client(configuration.hostname, configuration.port);
 
-    return       (game_loop(socket));
+    return       (window_loop(socket));
 }
 
-int             game_loop(int socket){
-    char        c;
-    request_t   request;
-    client_t    client;
+int             window_loop(int socket){
     SDL_Surface *window;
     SDL_Event   event;
-
-    int i, j, k, id, ret, reading;
-    fd_set      set;
     struct timeval tv;
+    fd_set      set;
+    client_t    client;
+    int ret;
+
 
     client.id = -1;
+    FD_ZERO(&set);
+    FD_SET(0, &set);
+    FD_SET(socket, &set);
+    tv.tv_sec = 0;
+    tv.tv_usec = 10;
     window = init_sdl();
+
+    if ((ret = select(socket + 1, &set, NULL, NULL, &tv)) < 0)
+        die("select()");
 
     while(event.type != SDL_QUIT) {
         SDL_PollEvent(&event);
-       /* if ((ret = select(socket + 1, &set, NULL, NULL, &tv)) < 0)
-            die("select()");
-
-        FD_ZERO(&set);
-        FD_SET(0, &set);
-        FD_SET(socket, &set);
-        tv.tv_sec = 0;
-        tv.tv_usec = 10;
-
-        if (ret)
-        {
-            if (FD_ISSET(0, &set))
-            {
-                c = getchar();
-                if (c == '.')
-                    graceful_exit(window);
-
-                if (c == 'z' || c == 'q' || c == 's' || c == 'd' || c == 'b')
-                {
-                    if (c == 'b')
-                        request.protocol = P_POSE_BOMB;
-                    else
-                    {
-                        request.protocol = P_MOVE;
-                        switch (c)
-                        {
-                            case 'z':
-                                request.direction = UP;
-                                break;
-                            case 's':
-                                request.direction = DOWN;
-                                break;
-                            case 'd':
-                                request.direction = RIGHT;
-                                break;
-                            default:
-                                request.direction = LEFT;
-                                break;
-                        }
-                    }
-                    write(socket, &request, sizeof(request_t));
-                }
-            }
-            if (FD_ISSET(socket, &set))
-            {
-                reading = read(socket, &request, sizeof(request_t));
-                if (reading == 0)
-                {
-                    printf("server is disconnect ....\n");
-                    fflush(stdout);
-                    close(socket);
-                    graceful_exit(window);
-                }
-                else
-                {
-                    if (request.protocol == P_CONNECT)
-                    {
-                        id = request.id;
-
-                        for (i = 0; i < MAX_CLIENTS; i++)
-                            if (request.clients[i].id == id)
-                                client = request.clients[i];
-                    }
-
-                    if (request.protocol == P_GAME_OVER)
-                    {
-                        printf("G A M E O V E R\n");
-                        fflush(stdout);
-                        graceful_exit(window);
-                    }
-
-                    if (request.protocol == P_UPDATE)
-                    {
-                        system("/bin/stty raw && /bin/stty cooked && clear");
-
-                        for (i = 0; i < 10; i++)
-                            if (request.clients[i].id == id && id > 0)
-                                client = request.clients[i];
-
-                        printf("............................\n");
-                        printf("....  B O M B E R M A N  ...\n");
-                        printf("............................\n");
-                        printf("Quit    : .\n");
-                        printf("Up      : z\n");
-                        printf("Right   : d\n");
-                        printf("Down    : s\n");
-                        printf("Left    : q\n\n");
-
-                        for (i = 0; i < MAP_ROW; i++)
-                        {
-                            for (j = 0; j < MAP_COL; j++)
-                            {
-                                int find = 0;
-                                for (k = 0; k < MAX_CLIENTS; k++)
-                                {
-                                    client_t o = request.clients[k];
-
-                                    if (o.id > 0 && o.posx == j && o.posy == i)
-                                    {
-                                        if (client.id == o.id)
-                                            printf(" X |");
-                                        else
-                                            printf(" x |");
-                                        find = 1;
-                                        break;
-                                    }
-                                }
-                                if (find == 0)
-                                    printf(" %c |", request.map[i][j]);
-                            }
-                            printf("\n");
-                            for (k = 0; k < MAP_COL; k++)
-                                printf(" _  ");
-                            printf("\n");
-                        }
-                        fflush(stdout);
-                    }
-                }
-            }
-        }*/
+        game_loop(window, ret, socket, client, set, tv);
     }
 
 
     graceful_exit(window);
+    return (0);
+}
+
+int             game_loop(SDL_Surface   *window,
+                          int           ret,
+                          int           socket,
+                          client_t      client,
+                          fd_set        set,
+                          struct timeval tv)
+{
+    char        c;
+    request_t   request;
+    int i, j, k, id, reading;
+
+    if (ret)
+    {
+        if (FD_ISSET(0, &set))
+        {
+            c = getchar();
+            if (c == '.')
+                graceful_exit(window);
+
+            if (c == 'z' || c == 'q' || c == 's' || c == 'd' || c == 'b')
+            {
+                if (c == 'b')
+                    request.protocol = P_POSE_BOMB;
+                else
+                {
+                    request.protocol = P_MOVE;
+                    switch (c)
+                    {
+                        case 'z':
+                            request.direction = UP;
+                            break;
+                        case 's':
+                            request.direction = DOWN;
+                            break;
+                        case 'd':
+                            request.direction = RIGHT;
+                            break;
+                        default:
+                            request.direction = LEFT;
+                            break;
+                    }
+                }
+                write(socket, &request, sizeof(request_t));
+            }
+        }
+        if (FD_ISSET(socket, &set))
+        {
+            reading = read(socket, &request, sizeof(request_t));
+            if (reading == 0)
+            {
+                printf("server is disconnect ....\n");
+                fflush(stdout);
+                close(socket);
+                graceful_exit(window);
+            }
+            else
+            {
+                if (request.protocol == P_CONNECT)
+                {
+                    id = request.id;
+
+                    for (i = 0; i < MAX_CLIENTS; i++)
+                        if (request.clients[i].id == id)
+                            client = request.clients[i];
+                }
+
+                if (request.protocol == P_GAME_OVER)
+                {
+                    printf("G A M E O V E R\n");
+                    fflush(stdout);
+                    graceful_exit(window);
+                }
+
+                if (request.protocol == P_UPDATE)
+                {
+                    system("/bin/stty raw && /bin/stty cooked && clear");
+
+                    for (i = 0; i < 10; i++)
+                        if (request.clients[i].id == id && id > 0)
+                            client = request.clients[i];
+
+                    printf("............................\n");
+                    printf("....  B O M B E R M A N  ...\n");
+                    printf("............................\n");
+                    printf("Quit    : .\n");
+                    printf("Up      : z\n");
+                    printf("Right   : d\n");
+                    printf("Down    : s\n");
+                    printf("Left    : q\n\n");
+
+                    for (i = 0; i < MAP_ROW; i++)
+                    {
+                        for (j = 0; j < MAP_COL; j++)
+                        {
+                            int find = 0;
+                            for (k = 0; k < MAX_CLIENTS; k++)
+                            {
+                                client_t o = request.clients[k];
+
+                                if (o.id > 0 && o.posx == j && o.posy == i)
+                                {
+                                    if (client.id == o.id)
+                                        printf(" X |");
+                                    else
+                                        printf(" x |");
+                                    find = 1;
+                                    break;
+                                }
+                            }
+                            if (find == 0)
+                                printf(" %c |", request.map[i][j]);
+                        }
+                        printf("\n");
+                        for (k = 0; k < MAP_COL; k++)
+                            printf(" _  ");
+                        printf("\n");
+                    }
+                    fflush(stdout);
+                }
+            }
+        }
+    }
     return (0);
 }
